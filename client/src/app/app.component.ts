b60a018ad19117a8d4dc4c7ebb367b7cbcec6541
alex-toy <alexei.80@hotmail.fr>
2024-03-30 10:47:37 +0100

    nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skinet';
+  products : any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/product').subscribe({
+      next: (response:any) => this.products = response.items,
+      error: response => console.log(response),
+      complete: () => console.log(this.products)
+    });
+  }
 }
